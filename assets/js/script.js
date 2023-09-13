a1f867ea740c1ee0a3885579192c34850237e177
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-13 15:42:40 +0200

    add controls on input
    if the field is empty,
    if the task is already in the array (even if they've differents upp/low,
    empty input after added todo,
    if there are no todos show a text

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ createApp({
    data() {
       return {
          todo: "",
+         present: false, // To check if a todo is already present
+         empty: false, // To check if the input field is empty
          todos: [
             {
                text: "Mangiare",
@@ -28,12 +30,44 @@ createApp({
       deleteTodo(i) {
          this.todos.splice(i, 1)
       },
-      addTodo(text){
-         this.todos.push({text, "done": false});
+
+      addTodo(string){
+         this.present = false; // set to reset the state
+         this.empty = false; // set to reset the state
+
+         if (string == "") {
+
+            this.empty = true;
+            // if empty added an element in HTML with v:if="empty"
+            
+         } else {
+
+            // cycle for each to cycle inside the objects in the array
+            // if input is present text propriety set present to true (also with different Uppercase or Lowercase)
+            this.todos.forEach(todo => {
+               if (todo.text.toLowerCase() === string.toLowerCase()) {
+                  this.present = true;
+               }         
+            });
+            
+            // if because we get n results because it's a cycle and we want only one answer
+            // if not present push an object in the array
+            if (this.present == false) {
+               this.todos.push({"text": string, "done": false});
+            }
+
+            // if present added an element in HTML with v:if="present"
+            // if true it appears (that's why we set this.present="false" at the beginning)
+         }
+
+         // empty input field
+         this.todo = "";
       }
 
    }
 }).mount('#app')
+
+
 
 /* 
 Bonus: 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
