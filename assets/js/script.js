8497ec7a520e46ff999b2ce4810cff102999be26
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-13 14:31:45 +0200

    milestone 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,15 +24,13 @@ createApp({
       }
    },
    methods: {
-
+      deleteTodo(i) {
+         this.todos.splice(i, 1)
+      }
    }
 }).mount('#app')
 
-/* Descrizione: Rifare l’esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-text, una stringa che indica il testo del todo
-done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-
-MILESTONE 1 Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+/* 
 
 MILESTONE 2 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
 
